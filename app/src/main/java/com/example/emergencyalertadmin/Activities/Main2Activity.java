5ba8c3012aa449aea0cc0b32d21d6b410c437518
Beren KOTANLI <berenkotanli@gmail.com>
2019-07-23 10:38:21 +0300

    Cikis yap kismi calisir hale geldi, user confirmation dialog eklendi

--- app/src/main/java/com/example/emergencyalertadmin/Activities/Main2Activity.java
+++ app/src/main/java/com/example/emergencyalertadmin/Activities/Main2Activity.java
@@ -1,13 +1,17 @@
 package com.example.emergencyalertadmin.Activities;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
@@ -21,16 +25,48 @@ import com.example.emergencyalertadmin.R;
 import com.example.emergencyalertadmin.Fragment.RemoveUserFragment;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import javax.annotation.Nullable;
 
 public class Main2Activity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
     FirebaseAuth auth;
     FirebaseUser user;
+     private RecyclerView mMainList;
+     private FirebaseFirestore mFirestore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+       /* mMainList=findViewById(R.id.main_list);
+        mFirestore=FirebaseFirestore.getInstance();
+        mFirestore.collection("Notifications").addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException e) {
+                if(e!=null){
+                    Log.d("TAG", "error: " + e.getMessage());
+                    for (DocumentChange doc : queryDocumentSnapshots.getDocuments()){
+                        if(doc.getType()==DocumentChange.Type.ADDED){
+                            String notification = doc.getDocument().getString("notification");
+                            Log.d("TAG","Notification: "+ notification);
+                        }
+
+
+                    }
+
+                }
+            }
+        });
+*/
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -57,6 +93,7 @@ public class Main2Activity extends AppCompatActivity
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
+            createDialog();
             super.onBackPressed();
         }
     }
@@ -112,12 +149,43 @@ public class Main2Activity extends AppCompatActivity
             ChangeFragment changeFragment = new ChangeFragment(this);
             changeFragment.change(new RemoveCategoryFragment());
         }else if (id == R.id.exit) {
-            auth.signOut();
+           createDialog();
+
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
 
         return true;
+    }
+
+    private void createDialog() {
+        AlertDialog.Builder alertDlg = new AlertDialog.Builder(this);
+        alertDlg.setMessage("Çıkış yapmak istediğinizden emin misiniz?");
+        alertDlg.setCancelable(false);
+
+        alertDlg.setPositiveButton("Evet", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Main2Activity.super.onBackPressed();
+                logout();
+            }
+        });
+        alertDlg.setNegativeButton("Iptal", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+
+
+            }
+        });
+        alertDlg.create().show();
+    }
+
+    public void logout(){
+        FirebaseAuth.getInstance().signOut();
+        Intent intent= new Intent(getApplicationContext(), MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
     }
 }
